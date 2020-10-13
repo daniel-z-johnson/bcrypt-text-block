aa650f687aa56c6c39a50bd145c3e9ab44f13c5a
daniel-z-johnson <danzaljohnson@gmail.com>
2020-10-12 21:37:57 -0600

    added bcrypt funcationality to page

--- main.go
+++ main.go
@@ -1,46 +1,85 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "time"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
 
-    "github.com/gorilla/mux"
-    "golang.org/x/crypto/bcrypt"
+	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var minuteCost int
 
+type BcryptTextBlock struct {
+	Bcrypt  string `json:"bcrypt"`
+	Message string `json:"message"`
+}
+
 func Home(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "It works")
+	fmt.Fprintln(w, "It works")
 }
 
 func favicon(w http.ResponseWriter, r *http.Request) {
-    http.Redirect(w, r, "/static/favicon.ico", http.StatusFound)
+	http.Redirect(w, r, "/static/favicon.ico", http.StatusFound)
+}
+
+func BcryptEndPoint(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	err := r.ParseForm()
+	if err != nil {
+		body := BcryptTextBlock{Message: err.Error()}
+		b, err := json.Marshal(body)
+		w.WriteHeader(http.StatusInternalServerError)
+		if err != nil {
+			return
+		}
+		w.Write(b)
+		return
+	}
+	// should only be one message block should have more error
+	block := r.Form["textBlock"][0]
+	fmt.Printf("block of text: %s\n", block)
+	bcryptText, err := bcrypt.GenerateFromPassword([]byte(block), minuteCost)
+	fmt.Printf("bcrypt: %s\n", bcryptText)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	body := BcryptTextBlock{Bcrypt: string(bcryptText)}
+	b, err := json.Marshal(body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println(err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+	return
 }
 
 func main() {
-    fmt.Println("Starting bcrypt-text-block")
-    findMinuteCost()
-    fmt.Println(minuteCost)
-    r := mux.NewRouter()
+	fmt.Println("Starting bcrypt-text-block")
+	findMinuteCost()
+	fmt.Println(minuteCost)
+	r := mux.NewRouter()
 
-    r.HandleFunc("/", Home)
-    r.HandleFunc("/favicon.ico", favicon)
-    r.PathPrefix("/static/").Handler(http.FileServer(http.Dir(".")))
-    http.ListenAndServe(":1117", r)
+	r.HandleFunc("/", Home).Methods("GET")
+	r.HandleFunc("/favicon.ico", favicon).Methods("GET")
+	r.HandleFunc("/bcrypt-this", BcryptEndPoint).Methods("POST")
+	r.PathPrefix("/static/").Handler(http.FileServer(http.Dir(".")))
+	http.ListenAndServe(":1117", r)
 }
 
 func findMinuteCost() {
-   start := time.Now()
-   bcrypt.GenerateFromPassword([]byte("password"), bcrypt.DefaultCost)
-   dur := time.Since(start)
-   cost := bcrypt.DefaultCost
-   for dur < time.Minute {
-       cost += 1
-       dur += dur
-   }
-   minuteCost = cost
+	start := time.Now()
+	bcrypt.GenerateFromPassword([]byte("password"), bcrypt.DefaultCost)
+	dur := time.Since(start)
+	cost := bcrypt.DefaultCost
+	for dur < time.Minute {
+		cost++
+		dur += dur
+	}
+	minuteCost = cost
 }
-
-
--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,16 @@
 var bcryptIt = function() {
     var textBlock = $("#textBlock").val();
     console.log(textBlock)
+    $.ajax({
+        url: "/bcrypt-this?textBlock="+textBlock,
+        method: "POST",
+    }).fail(function(data, status){
+        console.log("error: " + status);
+        console.log("data: " + data["message"]);
+    }).done(function(data, status){
+        console.log("success: " + status);
+        console.log("data: " + data["bcrypt"]);
+    });
 }
 
-$("#bcryptBtn").click(bcryptIt)
+$("#bcryptBtn").click(bcryptIt);
